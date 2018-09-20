eb9a7d6688bbacb8ebe3fbcd7b46cacaab553314
pmistry2 <parthm@expressandroid.com>
2018-09-20 19:06:24 -0400

    Added Login screen as a first task

---
+++ app/src/main/java/com/sam/samproject/viewmodels/BaseViewModel.java
@@ -0,0 +1,6 @@
+package com.sam.samproject.viewmodels;
+
+import android.arch.lifecycle.ViewModel;
+
+public abstract class BaseViewModel extends ViewModel {
+}
---
+++ app/src/main/java/com/sam/samproject/viewmodels/LoginViewModel.java
@@ -0,0 +1,47 @@
+package com.sam.samproject.viewmodels;
+
+import android.databinding.BindingAdapter;
+import android.databinding.InverseBindingAdapter;
+import android.databinding.InverseBindingListener;
+import android.databinding.ObservableField;
+import android.support.v7.widget.AppCompatSpinner;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+public class LoginViewModel extends BaseViewModel {
+    private String roles[];
+    private ObservableField<String> text;
+    public LoginViewModel() {
+        text = new ObservableField<>();
+    }
+    public ObservableField<String> getText() {
+        return text;
+    }
+    public String[] getRoles() {
+        return roles;
+    }
+    public void setRoles(String[] roles) {
+        this.roles = roles;
+    }
+    @InverseBindingAdapter(attribute = "selectedValue", event = "selectedValueAttrChanged")
+    public static String captureSelectedValue(AppCompatSpinner pAppCompatSpinner) {
+        return (String) pAppCompatSpinner.getSelectedItem();
+    }
+    @BindingAdapter(value = {"selectedValue", "selectedValueAttrChanged"}, requireAll = false)
+    public static void bindSpinnerData(AppCompatSpinner pAppCompatSpinner, String newSelectedValue, final InverseBindingListener newTextAttrChanged) {
+        pAppCompatSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                newTextAttrChanged.onChange();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        if (newSelectedValue != null) {
+            int pos = ((ArrayAdapter<String>) pAppCompatSpinner.getAdapter()).getPosition(newSelectedValue);
+            pAppCompatSpinner.setSelection(pos, true);
+        }
+    }
+}
