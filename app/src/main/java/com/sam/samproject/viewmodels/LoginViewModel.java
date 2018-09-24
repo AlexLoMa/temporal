7df5acabe43286e997ef65951fde9bba5af12709
Alavala <HAlavala@ad.harman.com>
2018-09-24 17:55:25 -0400

    [feature/personalbanker]- changed the flags

--- app/src/main/java/com/sam/samproject/viewmodels/LoginViewModel.java
+++ app/src/main/java/com/sam/samproject/viewmodels/LoginViewModel.java
@@ -74,12 +74,12 @@ public class LoginViewModel extends BaseViewModel {
 
         }else if(text.get().equals("Personal Banker + Financial Advisor")){
             Intent intent = new Intent(v.getContext(), PersonalBankerActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
             v.getContext().startActivity(intent);
 
         }else if(text.get().equals("Branch Manager")){
             Intent intent = new Intent(v.getContext(), BranchManagerActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
             v.getContext().startActivity(intent);
         }
     }
