411606d0021eb8b031da0530676812cc7c4b01b3
AlexLoMa <temporal.projects@gmail.com>
2018-10-07 19:05:22 -0400

    BaseFragment modified.

--- app/src/main/java/com/sam/samproject/base/BaseFragment.java
+++ app/src/main/java/com/sam/samproject/base/BaseFragment.java
@@ -7,14 +7,13 @@ package com.sam.samproject.base;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import dagger.android.support.DaggerFragment;
 
-public abstract class BaseFragment extends Fragment {
+public abstract class BaseFragment extends DaggerFragment {
     protected abstract int layoutResource();
 
     @Nullable
