8f901de94f567648019b7a8bebc0b885e192dc8f
hemanth <hemanth@gmail.com>
2018-11-21 08:06:57 -0500

    feature/newmortgageform - Added dialog for signature

--- app/src/main/java/com/sam/samproject/personalbanker/fragments/SignatureDialogFragment.java
+++ app/src/main/java/com/sam/samproject/personalbanker/fragments/SignatureDialogFragment.java
@@ -4,15 +4,17 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.github.gcacace.signaturepad.views.SignaturePad;
 import com.sam.samproject.R;
 
-public class SignatureDialogFragment extends DialogFragment {
+public class SignatureDialogFragment extends DialogFragment implements View.OnClickListener {
 
     private Button clearButton;
     private SignaturePad mSignaturePad;
@@ -21,14 +23,26 @@ public class SignatureDialogFragment extends DialogFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.signature_layout,container,false);
-        clearButton = view.findViewById(R.id.clearButton);
-        clearButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mSignaturePad.clear();
-            }
-        });
+        clearButton = view.findViewById(R.id.clear_button);
+        mSignaturePad = view.findViewById(R.id.signature_pad);
+        clearButton.setOnClickListener(this);
         return view;
 
+    }
+
+    @Override
+    public void onStart() {
+        getDialog().getWindow().setGravity(Gravity.CENTER_HORIZONTAL);
+        WindowManager.LayoutParams params = getDialog().getWindow().getAttributes();
+        params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        getDialog().getWindow().setAttributes(params);
+        super.onStart();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        mSignaturePad.clear();
     }
 }
